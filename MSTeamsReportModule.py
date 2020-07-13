037d13c8b9597615da9a7c184cf992affa63ea8c
Guilherme Beco <2171293@my.ipleiria.pt>
2020-07-13 15:57:56 +0100

    Patricio sem acento

--- MSTeamsReportModule.py
+++ MSTeamsReportModule.py
@@ -1,62 +1,19 @@
-#         #Post a message to the ingest messages in box.
-#         message = IngestMessage.createMessage(IngestMessage.MessageType.DATA,
-#             "Microsoft Teams data", "Found %d files" % fileCount)
-#         IngestServices.getInstance().postMessage(message)
-
-#         return IngestModule.ProcessResult.OK
-
-# Sample module in the public domain. Feel free to use this as a template
-# for your modules (and you can remove this header and take complete credit
-# and liability)
+#   LabCif-MSTeams: This project extracts MSTeams Artifacts
+#   Copyright (C) 2020
+#   Authors: Guilherme Beco and Tiago Garcia
+#   Mentors: Miguel Monteiro de Sousa Frade and Patricio Rodrigues Domingues
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   any later version.
 #
-# Contact: Brian Carrier [carrier <at> sleuthkit [dot] org]
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
 #
-# This is free and unencumbered software released into the public domain.
-#
-# Anyone is free to copy, modify, publish, use, compile, sell, or
-# distribute this software, either in source code form or as a compiled
-# binary, for any purpose, commercial or non-commercial, and by any
-# means.
-#
-# In jurisdictions that recognize copyright laws, the author or authors
-# of this software dedicate any and all copyright interest in the
-# software to the public domain. We make this dedication for the benefit
-# of the public at large and to the detriment of our heirs and
-# successors. We intend this dedication to be an overt act of
-# relinquishment in perpetuity of all present and future rights to this
-# software under copyright law.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-
-# See http://sleuthkit.org/autopsy/docs/api-docs/latest/index.html for documentation
-
-# Simple report module for Autopsy.
-# Used as part of Python tutorials from Basis Technology - September 2015
-#
-# Writes a CSV file with all file names and MD5 hashes.
-
-#     LabCif-MSTeams: This project extracts MSTeams Artifacts
-#     Copyright (C) 2020
-#     Authors: Guilherme Beco and Tiago Garcia
-#     Mentors: Miguel Monteiro de Sousa Frade and Patrício Rodrigues Domingues
-#     This program is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU General Public License as published by
-#     the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-#
-#     This program is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU General Public License for more details.
-#
-#     You should have received a copy of the GNU General Public License
-#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 import os
